2526e85487b1182be9094d3df998ee2734565d03
romtroy <roman.troyitski@gmail.com>
2020-02-12 23:58:08 +0300

    Second version

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -1,18 +1,17 @@
 public class ConditionalStatement {	
 	public static void main(String[] args) {
 		int age = 22, height = 175;
-		boolean human = false;
-		char firstLetterOfName = 111;
-
+				
 		if (age > 20) {
 			System.out.println(age + " more than 20");
 		}
-	
-		if (human == true) {
+		
+		boolean isItMan = false;
+		if (isItMan) {
 			System.out.println(" man");
 		}
 		
-		if (human == false) {
+		if (!isItMan) {
 			System.out.println(" women");
 		}
 		
@@ -21,7 +20,7 @@ public class ConditionalStatement {
 		} else {
 			System.out.println("higher 180");
 		}
-
+		char firstLetterOfName = 111;
 		if (firstLetterOfName == 109) {
 			System.out.println("first letter of name is M");
 		} else if (firstLetterOfName == 111) {
