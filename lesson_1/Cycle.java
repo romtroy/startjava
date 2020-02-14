768927110e084aee83c6749f36b3bc26506f0f7b
romtroy <roman.troyitski@gmail.com>
2020-02-15 01:21:49 +0300

    first version

---
+++ lesson_1/Cycle.java
@@ -0,0 +1,27 @@
+public class Cycle {	
+	public static void main(String[] args) {
+		for (int i = 0; i <= 20; i++){
+			System.out.print(" " + i);
+		}
+
+		System.out.println(" ");
+
+		int j = 6;
+		while (j >= -6){
+			System.out.print(" " + j);
+			j = j - 2;
+		}
+
+		System.out.println(" ");
+
+		int x = 10;
+		int sum =0;
+		do {
+			if (x % 2 != 0) {
+				sum = sum + x;
+			}
+			x++;
+		} while(x <= 20);  
+		System.out.println(sum);
+	}
+}
