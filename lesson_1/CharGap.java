4c25fb0566f3e441eb9e6f9dbac1288cceb232ca
romtroy <roman.troyitski@gmail.com>
2020-05-01 12:45:00 +0300

    first commit

--- lesson_1/CharGap.java
+++ lesson_1/CharGap.java
@@ -5,8 +5,6 @@ public class CharGap {
 		do {
 			System.out.print(" " + sign);
 			sign++;
-			} while (sign < '\u0126');
+		} while (sign < '\u0126');
 	} 
 }
-
-//Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178]. Если выводятся кракозябры, то [33, 126]
