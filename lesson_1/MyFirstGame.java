ceebdaaf2119b643876de9b388c80ad3691dc73e
romtroy <roman.troyitski@gmail.com>
2020-05-05 17:55:32 +0300

    third commit

--- lesson_1/MyFirstGame.java
+++ lesson_1/MyFirstGame.java
@@ -1,18 +1,17 @@
 public class MyFirstGame {	
 	public static void main(String[] args){
 		int numAnswer = 51;
-		System.out.println("Enter a number between 0 and 100");
 		int numUser = 100;
+
+		System.out.println("Enter a number between 0 and 100");
 		if(numUser != numAnswer) {
 			System.out.println("You lose!");
 			if(numUser > numAnswer) {
 				System.out.println("The number that you entered is greater than what it is");
-			}
-			else if(numUser < numAnswer) {
+			} else if(numUser < numAnswer) {
 				System.out.println("The number that you entered is less than what it is");
 			}
-		}
-		else if(numUser == numAnswer) {
+		} else if(numUser == numAnswer) {
 			System.out.println("You guess! " + numAnswer);
 		}
 	}
