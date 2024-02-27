3ce311769ca55ea0135f6e3da57547b29e0b3bf3
romtroy <roman.troyitski@gmail.com>
2024-02-27 12:14:39 +0300

    4я правка переменные

--- lesson_1/VariablesTheme.java
+++ lesson_1/VariablesTheme.java
@@ -34,7 +34,6 @@ public class VariablesTheme {
         System.out.println("   J    a  v     v  a\n   J   a a  v   v  a a");
         System.out.println("J  J  aaaaa  V V  aaaaa\n JJ  a     a  V  a     a\n");
         
-        
         System.out.println("4. Вывод min и max значений целых числовых типов");
         byte byteMaxNumber = 127;
         short shortMaxNumber = 32767;
