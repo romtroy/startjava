51dbb9536be10b5345afc367b211d6ecbcb96f03
romtroy <roman.troyitski@gmail.com>
2024-10-31 15:59:57 +0300

    version 21/10/24

--- lesson_2/Person.java
+++ lesson_2/Person.java
@@ -1,18 +1,31 @@
-class Person {	
-	String gender = "women";
-	float weight = 55.5f;
-	float height = 170.6f;
-	int age = 43;
-	
-	void move() {
-	}
-
-	void eat() {
-	}
-
-	void work() {
-	}
-
-	void learnJava() {
-	}
+public class Person {
+    String gender;
+    String name;
+    int height;
+    int weight;
+    int age;
+    
+    public Person(String gender, String name, int height, int weight, int age) {
+        this.gender = gender;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+    }
+    Person ivan = new Person("male", "Ivan", 175, 75, 75); 
+    void walk() {
+        System.out.println("walk");
+    }
+    void sit() {
+        System.out.println("sit");
+    }
+    void run() {
+        System.out.println("run");
+    }
+    void talk() {
+        System.out.println("talk");
+    }
+    void learnJava() {
+        System.out.println("learn Java");
+    }
 }
