3e63d025e0b04f788cff0430a4a5d33500f5031e
romtroy <roman.troyitski@gmail.com>
2024-11-21 18:24:17 +0300

    21.11.24

--- lesson_2/Calculator.java
+++ lesson_2/Calculator.java
@@ -1,76 +1,75 @@
 public class Calculator {
  
-        private int number1;
-        private int number2;
-        private char sign;
-        private int result;
-        
-        public void firstNumber() {
-            System.out.print("Введите первое число: " );
-        }
-        public void sighMath() {
-            System.out.print("Введите знак операции (+, -, *, /, ^, %): " );
-        }
-        
-        public void secondNumber() {
-            System.out.print("Введите второе число: " );
-        }
-        
-        /*public int getNumber1() {
-            return number1;
-        }
-         */
-        public void setNumber1(int number1) {
-            this.number1 = number1;     
-        }
-       
-        /*public int getNumber2() {
-            return number2;
-        }*/
-        
-        public void setNumber2(int number2) {
-            this.number2 = number2;     
-        }
-        
-        public char getSign() {
-            return sign;
-        }
-        
-        public void setSign(char sign) {
-            this.sign = sign;     
-        }
-        
-        public void calculate() {
-            switch (sign) {
-                case '+':
-                    result = number1 + number2;
+    private int number1;
+    private int number2;
+    private char sign;
+    private int result;
+    
+    public void firstNumber() {
+        System.out.print("Введите первое число: ");
+    }
+    
+    public void sighMath() {
+        System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
+    }
+    
+    public void secondNumber() {
+        System.out.print("Введите второе число: ");
+    }
+    
+    /* public int getNumber1() {
+        return number1;
+    }
+    */
+    public void setNumber1(int number1) {
+        this.number1 = number1;     
+    }
+    
+    /* public int getNumber2() {
+        return number2;
+    } */
+    
+    public void setNumber2(int number2) {
+        this.number2 = number2;     
+    }
+    
+    public char getSign() {
+        return sign;
+    }
+    
+    public void setSign(char sign) {
+        this.sign = sign;     
+    }
+    
+    public void calculate() {
+        switch (sign) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                if (number2 == 0) {
+                    System.out.println("Ошибка: деление на ноль запрещено");
                     break;
-                case '-':
-                    result = number1 - number2;
-                    break;
-                case '*':
-                    result = number1 * number2;
-                    break;
-                case '/':
-                    if(number2 == 0) {
-                        System.out.println("Ошибка: деление на ноль запрещено");
-                        break;
-                    } else { 
-                        result = number1 / number2;
-                    }
-                    break;
-                case '^':
-                    result = 1;
-                    for (int j = 1; j <= number2; j++) {
-                        result *= number1;
-                    }
-                    break;
-                case '%':
-                    result = number1 % number2;
-                    break;
-            }
-
-            System.out.println(number1 + " " + sign + " " + number2 + " = " + result);
-
+                } else { 
+                    result = number1 / number2;
+                }
+                break;
+            case '^':
+                result = 1;
+                for (int j = 1; j <= number2; j++) {
+                    result *= number1;
+                }
+                break;
+            case '%':
+                result = number1 % number2;
+                break;
         }
+        System.out.println(number1 + " " + sign + " " + number2 + " = " + result);
+    }
 }
--- lesson_2/CalculatorTest.java
+++ lesson_2/CalculatorTest.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
-    
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
@@ -9,34 +8,30 @@ public class CalculatorTest {
         String signContinue = "yes";
         
         do {
-            calc.firstNumber(); //вызываем метод ввода
+            calc.firstNumber(); // вызываем метод ввода
             int number1 = scan.nextInt();
             calc.setNumber1(number1); // делаем 1ю цифру
             
             calc.sighMath();
             char sign = scan.next().charAt(0);
-            while((sign != '+') && (sign != '-') && (sign != '*') && (sign != '/') && (sign != '^') && (sign != '%')) {
+            while ((sign != '+') && (sign != '-') && (sign != '*') && (sign != '/') && (sign != '^') && (sign != '%')) {
                 System.out.println("Ошибка: операция " + sign + " не поддерживается ");
                 calc.sighMath();
                 sign = scan.next().charAt(0);
             }
             calc.setSign(sign);
             
-            calc.secondNumber(); //вызываем метод ввода
+            calc.secondNumber(); // вызываем метод ввода
             int number2 = scan.nextInt();
             calc.setNumber2(number2); // делаем 2ю цифру
             
-            calc.calculate(); //вызываем метод расчёта
-            //String signContinue = scan.next();
+            calc.calculate(); // вызываем метод расчёта
             System.out.println("Хотите продолжить вычисления? [yes/no] ");
             signContinue = scan.next();
-            while(!signContinue.equals("yes") && !signContinue.equals("no")) {
+            while (!signContinue.equals("yes") && !signContinue.equals("no")) {
                 System.out.println("Хотите продолжить вычисления? [yes/no] ");
                 signContinue = scan.next();
             }
-        } while(signContinue.equals("yes") || !signContinue.equals("no"));
-
+        } while (signContinue.equals("yes") || !signContinue.equals("no"));
     }   
 }
-
-
