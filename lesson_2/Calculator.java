daa28cebc8d732cf42aebc87ce96261b6a8a9d40
romtroy <roman.troyitski@gmail.com>
2024-11-22 12:31:55 +0300

    21.11.24

--- lesson_2/Calculator.java
+++ lesson_2/Calculator.java
@@ -17,17 +17,9 @@ public class Calculator {
         System.out.print("Введите второе число: ");
     }
     
-    /* public int getNumber1() {
-        return number1;
-    }
-    */
     public void setNumber1(int number1) {
         this.number1 = number1;     
     }
-    
-    /* public int getNumber2() {
-        return number2;
-    } */
     
     public void setNumber2(int number2) {
         this.number2 = number2;     
