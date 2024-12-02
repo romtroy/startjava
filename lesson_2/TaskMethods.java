d45bea70136f6dc20b5811683024ae604128a032
romtroy <roman.troyitski@gmail.com>
2024-12-02 20:35:40 +0300

    Задание на именование методов

---
+++ lesson_2/TaskMethods.java
@@ -0,0 +1,31 @@
+public class TaskMethods {
+    public static void main(String[] args) {
+        TaskMethodsNoBoolean part1 = new TaskMethodsNoBoolean();
+        part1.findMostLongWord();
+        part1.chooseMenuEditorMacOs();
+        part1.calculateAverageRating();
+        part1.countUniqueWords();
+        part1.showError();
+        part1.syncCloudStorage();
+        part1.restoreBackup();
+        part1.stopDownloadAria();
+        part1.resetVacuumMi();
+        part1.recFileToFlash();
+        part1.convertCelsiusFahrenheit();
+        part1.enterMathExpression3Arg();
+        part1.findWinnerNfs();
+        part1.findBookByName();
+        
+        TaskMethodsBoolean part2 = new TaskMethodsBoolean();
+        part2.doesContinue();
+        part2.isDeletedFromHddOrFlash();
+        part2.isContainUniqueNumber();
+        part2.isLetterOrSmt();
+        part2.isEqualDigits();
+        part2.areAttemptsMarioGame();
+        part2.isEmptyOrSpaces();
+        part2.isEvenNumberTurnPc();
+        part2.isValidPathToFile();
+        part2.isFileExist();
+    }
+}
