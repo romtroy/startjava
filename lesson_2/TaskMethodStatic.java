ad723ad37184dbe4e3092973febd62708e3296c1
romtroy <roman.troyitski@gmail.com>
2024-12-05 00:04:42 +0300

    dell

--- lesson_2/TaskMethodStatic.java
+++
@@ -1,7 +0,0 @@
-public class TaskMethodStatic {
-    String methodName;
-    
-    public static String getMethodName() {
-        return Thread.currentThread().getStackTrace()[2].getMethodName();
-    }
-}
