77e5c269b9f83b9cfd7cfac73b4a090e0aea6f79
romtroy <roman.troyitski@gmail.com>
2025-02-26 00:04:35 +0300

    first task array

--- src/com/startjava/lesson_2_3_4/array/ReverseArray.java
+++ src/com/startjava/lesson_2_3_4/array/ReverseArray.java
@@ -3,31 +3,37 @@ package com.startjava.lesson_2_3_4.array;
 public class ReverseArray {
     public static void main(String[] args) {
 
-        int[] firstTaskArr = {1, 2, 3, 4, 5, 0};
-        // int[] firstTaskArr = {};
-        // int[] firstTaskArr = null;
+        int[] firstTaskArr1 = {};
+        int[] firstTaskArr2 = null;
+        int[] firstTaskArr3 = {6, 8, 9, 1};
+        int[] firstTaskArr4 = {13, 8, 5, 3, 2, 1, 1};
 
-        if (firstTaskArr == null) {
+        workWithArray(firstTaskArr1);
+        workWithArray(firstTaskArr2);
+        workWithArray(firstTaskArr3);
+        workWithArray(firstTaskArr4);
+    }
+
+    private static void workWithArray(int[] numbers) {
+        if (numbers == null) {
             System.out.println("Массив равен null");
             return;
-        } else if (firstTaskArr.length == 0) {
+        } else if (numbers.length == 0) {
             System.out.println("Пустой массив");
             return;
         }
-
-        displayArray("   До реверса", firstTaskArr);
-
-        arrayReverse(firstTaskArr);
-
-        displayArray("После реверса", firstTaskArr);
+        displayArray("   До реверса", numbers);
+        arrayReverse(numbers);
+        displayArray("После реверса", numbers);
     }
 
-    public static void arrayReverse(int[] firstTaskArr) {
-        int left = 0, right = firstTaskArr.length - 1;
+    public static void arrayReverse(int[] numbers) {
+        int left = 0;
+        int right = numbers.length - 1;
         while (left < right) {
-            int temp = firstTaskArr[left];
-            firstTaskArr[left] = firstTaskArr[right];
-            firstTaskArr[right] = temp;
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
             left++;
             right--;
         }
