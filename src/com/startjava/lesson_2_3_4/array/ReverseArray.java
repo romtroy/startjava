3a2fc954dc5c997cf14609373f8a3c87cc0013d5
romtroy <roman.troyitski@gmail.com>
2025-03-12 19:44:31 +0300

    12-03-25

--- src/com/startjava/lesson_2_3_4/array/ReverseArray.java
+++ src/com/startjava/lesson_2_3_4/array/ReverseArray.java
@@ -7,26 +7,17 @@ public class ReverseArray {
         int[] array3 = {6, 8, 9, 1};
         int[] array4 = {13, 8, 5, 3, 2, 1, 1};
 
-        checkArray(arrayEmpty);
-        checkArray(arrayNull);
-        checkArray(array3);
-        checkArray(array4);
+        displayResult(arrayEmpty);
+        displayResult(arrayNull);
+        displayResult(array3);
+        displayResult(array4);
     }
 
-    private static void checkArray(int[] numbers) {
-        if (numbers == null) {
-            System.out.println("Массив равен null");
-            return;
-        } else if (numbers.length == 0) {
-            System.out.println("Пустой массив");
+    private static void reverseArray(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
             return;
         }
-        displayArray("   До реверса:", numbers);
-        reverseArray(numbers);
-        displayArray("После реверса:", numbers);
-    }
 
-    public static void reverseArray(int[] numbers) {
         int left = 0;
         int right = numbers.length - 1;
         while (left < right) {
@@ -38,8 +29,27 @@ public class ReverseArray {
         }
     }
 
-    public static void displayArray(String message, int[] numbers) {
-        System.out.print(message + " [ ");
+    private static void displayResult(int[] numbers) {
+        if (numbers == null) {
+            System.out.println("Массив равен null");
+            return;
+        }
+        if (numbers.length == 0) {
+            System.out.println("Пустой массив");
+            return;
+        }
+
+        System.out.print("   До реверса: ");
+        printArray(numbers);
+
+        reverseArray(numbers); //или сам вызов этого метода необходимо в main перенести ?
+
+        System.out.print("После реверса: ");
+        printArray(numbers);
+    }
+
+    private static void printArray(int[] numbers) {
+        System.out.print("[ ");
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i]);
             if (i < numbers.length - 1) {
